089d52c45fb294f0b5ba9f97f59ad5c1b16a403e
RedWings <korylinkin@gmail.com>
2017-02-03 00:27:34 -0300

    cambiado el width del responsive hd en 1379 el max-width  para que haya una division mas clara en fhd

--- app/config.inc.php
+++ app/config.inc.php
@@ -31,6 +31,6 @@ define('RUTA_CSS', SERVIDOR .'css/');
 define('RUTA_JS', SERVIDOR .'js/');
 //RUTAS RESPONSIVE
 define('CSS_FHD','<link rel="stylesheet" type="text/css" media="(min-width:1380px)" href="'.RUTA_CSS.'responsive/responsive_fhd.css">');
-define('CSS_HD','<link rel="stylesheet" type="text/css" media="(min-width:768px) and (max-width:1380px)" href="'.RUTA_CSS.'responsive/responsive_hd.css">');
+define('CSS_HD','<link rel="stylesheet" type="text/css" media="(min-width:768px) and (max-width:1379px)" href="'.RUTA_CSS.'responsive/responsive_hd.css">');
 define('CSS_MOBIL','<link rel="stylesheet" type="text/css" media="(min-width:320px) and (max-width:468px)" href="'.RUTA_CSS.'responsive/responsive_m.css">');
 define('IMPORTAR_RESPONSIVE',CSS_FHD.PHP_EOL.CSS_HD.PHP_EOL.CSS_MOBIL);
